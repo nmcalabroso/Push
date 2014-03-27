5379d98a45f8108f8cd006ce342ef10e246cf250
= <=>
2014-03-28 06:50:29 +0800

    Change color of text box.

--- client/gui.py
+++ client/gui.py
@@ -69,7 +69,7 @@ class Rectangle(object):
     def __init__(self, x1, y1, x2, y2, batch):
         self.vertex_list = batch.add(4, pyglet.gl.GL_QUADS, None,
             ('v2i', [x1, y1, x2, y1, x2, y2, x1, y2]),
-            ('c4B', [200, 200, 220, 255] * 4)
+            ('c4B', [236, 240, 241, 255] * 4)
         )
 
 class TextWidget(UIObject):
